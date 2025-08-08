47cdcdebae17f415be588fbdb7c7ad529d0cfd31
ashish-karigar <karigarashish@gmail.com>
2025-08-08 16:23:56 +0530

    windows executable

--- ASTRA/src/astra.py
+++ ASTRA/src/astra.py
@@ -1,12 +1,33 @@
 # astra.py
+import os
+import sys
 import tkinter as tk
 from intro import IntroFrame
 from main import MainFrame
+from PIL import ImageTk, Image
 
+
+def resource_path(self, relative_path):
+    """Get absolute path to resource, works for dev and PyInstaller"""
+    try:
+        # PyInstaller stores files in a temp folder called _MEIPASS
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        try:
+            icon_image = Image.open(resource_path('logo.ico'))
+            icon_photo = ImageTk.PhotoImage(icon_image)
+            self.iconphoto(False, icon_photo)
+        except Exception as e:
+            print(f"Failed to set window icon: {e}")
+
         self.title("ASTRA – Advanced Sync & Tracking for Replication Automation")
         self.attributes('-fullscreen', True)
 
